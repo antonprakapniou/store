2390ea4fd8c897e64f81a91661998db450dfcf10
“Anton <antonprokapnev@gmail.com>
2022-11-27 15:37:25 +0300

    order header & details models created

---
+++ Store/Models/OrderDetails.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Store.Models
+{
+    public class OrderDetails
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int OrderHeaderId { get; set; }
+
+        [ForeignKey("OrderHeaderId")]
+        public OrderHeader? OrderHeader { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product? Product { get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Store/Models/OrderHeader.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Store.Models
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? CreatedByUserId { get; set; }
+
+        [ForeignKey("CreatedByUserId")]
+        public IdentityUser? CreatedBy { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public DateTime ShippingDate { get; set; }
+
+        [Required]
+        public double FinalOrderTotal { get; set; }
+        public string? OrderStatus { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string? TransactionId { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+    }
+}
