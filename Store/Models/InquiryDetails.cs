6ad54e342ab1701828accb719548508606c970e4
“Anton <antonprokapnev@gmail.com>
2022-11-27 15:30:35 +0300

    inquiry header & details models created

---
+++ Store/Models/InquiryDetails.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Store.Models
+{
+    public class InquiryDetails
+    {
+        [Key]
+        public int Id { get; set; }
+        public required int InquiryHeaderId { get; set; }
+
+        [ForeignKey("InquiryHeaderId")]
+        public InquiryHeader? InquiryHeader { get; set; }
+        public required int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product? Product { get; set; }
+    }
+}
---
+++ Store/Models/InquiryHeader.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Store.Models
+{
+    public class InquiryHeader
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public IdentityUser? User { get; set; }
+        public DateTime InquiryDate { get; set; }
+        public required string? Email { get; set; }
+    }
+}
