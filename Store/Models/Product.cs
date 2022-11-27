2af66985efa9ec9259167fc1eca268ab156e08bf
“Anton <antonprokapnev@gmail.com>
2022-11-27 15:23:04 +0300

    product model created

---
+++ Store/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Store.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Range(0.1, double.MaxValue, ErrorMessage = "The price value is invalid")]
+        public double Price { get; set; }
+
+        public string? Description { get; set; }
+
+        [Display(Name = "Category Type")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category? Category { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        [NotMapped]
+        [Range(1, 10000)]
+        public int Temp { get; set; }
+
+        public Product()
+        {
+            Temp=1;
+        }
+    }
+}
