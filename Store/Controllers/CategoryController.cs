972a478d68af1787cd60de9978fc5cb7660aa8b5
“Anton <antonprokapnev@gmail.com>
2022-11-27 17:21:13 +0300

    index for category controller created

--- Store/Controllers/CategoryController.cs
+++ Store/Controllers/CategoryController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Store.Data;
+using Store.Data.Repositories.IRepositories;
+using Store.Models;
+using System.Data;
 
 namespace Store.Controllers
 {
+    [Authorize(Roles = WebConstants.AdminRole)]
     public class CategoryController : Controller
     {
+        private readonly ICategoryRepository _categoryRepo;
+
+        public CategoryController(ICategoryRepository categoryRepo)
+        {
+            _categoryRepo=categoryRepo;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> categories = _categoryRepo.FindAll();
+            return View(categories);
         }
     }
 }
