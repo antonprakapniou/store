ac8910d7d4bfedf67037f06363555b879e659251
“Anton <antonprokapnev@gmail.com>
2022-11-27 18:55:11 +0300

    index for inquiry controller added

--- Store/Controllers/InquiryController.cs
+++ Store/Controllers/InquiryController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Store.Data;
+using Store.Data.Repositories.IRepositories;
+using Store.Models.ViewModels;
+using System.Data;
 
 namespace Store.Controllers
 {
+    [Authorize(Roles = WebConstants.AdminRole)]
     public class InquiryController : Controller
     {
-        public IActionResult Index()
+        private readonly IInquiryHeaderRepository _inquiryHeaderRepo;
+        private readonly IInquiryDetailsRepository _inquiryDetailsRepo;
+
+        [BindProperty]
+        public InquiryViewModel? InquiryViewModel { get; set; }
+
+        public InquiryController(IInquiryHeaderRepository inquiryHeaderRepository, IInquiryDetailsRepository inquiryDetailsRepository)
         {
-            return View();
+            _inquiryDetailsRepo= inquiryDetailsRepository;
+            _inquiryHeaderRepo= inquiryHeaderRepository;
         }
+
+        public IActionResult Index() => View();
     }
 }
