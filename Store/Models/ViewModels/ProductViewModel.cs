db526e41db2ce1b1156f433b7ab96c88e0229471
“Anton <antonprokapnev@gmail.com>
2022-11-27 15:50:32 +0300

    product view model created

---
+++ Store/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Store.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Product? Product { get; set; }
+        public IEnumerable<SelectListItem>? CategorySelectList { get; set; }
+    }
+}
