a1bc08bb73b3b6a2b93d0cc9f5c4877cc73bc28e
“Anton <antonprokapnev@gmail.com>
2022-11-27 15:53:05 +0300

    details view model created

---
+++ Store/Models/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Store.Models.ViewModels
+{
+    public class DetailsViewModel
+    {
+        public Product Product { get; set; }
+        public bool ExistsInCart { get; set; }
+
+        public DetailsViewModel()
+        {
+            Product= new();
+        }
+    }
+}
