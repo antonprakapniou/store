e8dc50ab0b048386314a4ec98e999fba124b92a7
“Anton <antonprokapnev@gmail.com>
2022-11-27 15:26:15 +0300

    product table added to db

--- Store/Data/AppDbContext.cs
+++ Store/Data/AppDbContext.cs
@@ -7,6 +7,7 @@ namespace Store.Data
     public class AppDbContext:IdentityDbContext
     {
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     }
