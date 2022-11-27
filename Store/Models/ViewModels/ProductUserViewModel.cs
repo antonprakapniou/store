ba2c4a76f81e62af430e83bd75c22d53989468fc
“Anton <antonprokapnev@gmail.com>
2022-11-27 15:51:54 +0300

    product user view modek created

---
+++ Store/Models/ViewModels/ProductUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Store.Models.ViewModels
+{
+    public class ProductUserViewModel
+    {
+        public IdentityUser? User { get; set; }
+        public List<Product>? ProductList { get; set; }
+
+        public ProductUserViewModel()
+        {
+            ProductList=new();
+        }
+    }
+}
