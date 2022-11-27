9e5e83842ee6454b4420c1f04cfa15a07758d96c
“Anton <antonprokapnev@gmail.com>
2022-11-27 10:46:38 +0300

    scaffold identity tables added to db

--- Store/Program.cs
+++ Store/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Store.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionName = "DefaultConnection";
+var connectionString = builder
+    .Configuration
+    .GetConnectionString(connectionName)??
+    throw new InvalidOperationException($"Connection string {connectionName} not found.");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
@@ -15,11 +26,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
