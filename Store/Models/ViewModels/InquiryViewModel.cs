33d8cad657bb3b663ad9a667fae2dcbbe5f14d00
“Anton <antonprokapnev@gmail.com>
2022-11-27 15:59:42 +0300

    inquiry view model created

---
+++ Store/Models/ViewModels/InquiryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Store.Models.ViewModels
+{
+    public class InquiryViewModel
+    {
+        public InquiryHeader? InquiryHeader { get; set; }
+        public IEnumerable<InquiryDetails>? InquiryDetails { get; set; }
+    }
+}
