3a0e8aadda1b8e25b11a4f1501fc208198c78b18
“Anton <antonprokapnev@gmail.com>
2022-11-28 10:52:51 +0300

    order list view model added

---
+++ Store/Models/ViewModels/OrderListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Store.Models.ViewModels
+{
+    public class OrderListViewModel
+    {
+        public IEnumerable<OrderHeader>? OrderHeaderList { get; set; }
+        public IEnumerable<SelectListItem>? StatusList { get; set; }
+        public string? Status { get; set; }
+    }
+}
