077e4002dffc2eb90a25a7faa95e2350ebd35cbb
“Anton <antonprokapnev@gmail.com>
2022-11-28 10:49:15 +0300

    order controller created

---
+++ Store/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Data.Repositories.IRepositories;
+using Store.Utilities.Braintree;
+
+namespace Store.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderHeaderRepository _orderHeaderRepo;
+        private readonly IOrderDetailsRepository _orderDetailsRepo;
+        private readonly IBraintreeGate _braintree;
+
+        public OrderController(
+            IOrderHeaderRepository orderHeaderRepo,
+            IOrderDetailsRepository orderDetailsRepo,
+            IBraintreeGate braintree)
+        {
+            _orderHeaderRepo= orderHeaderRepo;
+            _orderDetailsRepo= orderDetailsRepo;
+            _braintree= braintree;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
